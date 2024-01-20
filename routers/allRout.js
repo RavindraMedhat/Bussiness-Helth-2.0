ed020c127a71745a7b94a188c6ae5e45336689eb
Ravindrasinh kiritsinh medhat <ravindrasinhmedhat@gmail.com>
2024-01-20 12:20:10 +0530

    calculator in progress

--- routers/allRout.js
+++ routers/allRout.js
@@ -9,44 +9,52 @@ const app = express();
 
 
 
-const verifyUser = (req,res,next)=>{
+const verifyUser = (req, res, next) => {
     const t = req.cookies.BHC;
-    if(t){
-        jwt.verify(t,"RKM",(err,decode)=>{
-            if(err){
+    if (t) {
+        jwt.verify(t, "RKM", (err, decode) => {
+            if (err) {
                 res.redirect("/user/login");
             }
             req.data = decode.data;
             next();
-    });
-    }else{
+        });
+    } else {
         res.redirect("/user/login");
     }
 }
 
-const verifyAdmin = (req,res,next)=>{
+const verifyAdmin = (req, res, next) => {
     const t = req.cookies.BHC;
-    if(t){
-        jwt.verify(t,"RKM",(err,decode)=>{
-            if(err || decode.data.Roal != "Admin"){
+    if (t) {
+        jwt.verify(t, "RKM", (err, decode) => {
+            if (err || decode.data.Roal != "Admin") {
                 res.redirect("/Company/companyList");
             }
             req.data = decode;
             next();
-    });
-    }else{
+        });
+    } else {
         res.redirect("/user/login");
     }
 }
 
-app.use("/user",userRout);
-app.use("/detail",detailsRout);
-app.use("/profile",verifyUser,profileRout);
-app.use("/Company",verifyUser,companyRout);
-app.use("/indicator",verifyAdmin,indicatorRout);
+app.use("/user", userRout);
+app.use("/detail", detailsRout);
+app.use("/profile", verifyUser, profileRout);
+app.use("/Company", verifyUser, companyRout);
+app.use("/indicator", verifyAdmin, indicatorRout);
 
-app.use((req,res)=>{
+app.get('/InvestFlow_Navigator',verifyUser, (req, res) => {
+
+    res.render('InvestFlow_Navigator', {
+        defaultInvestment: 100000,
+        defaultIncomeTarget: 60000000
+    });
+});
+
+app.use((req, res) => {
     console.log(req.url);
     res.redirect("/Company/companyList");
 })
-module.exports=app;
+module.exports = app;
